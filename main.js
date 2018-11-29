f023231fee86248139eef7939d6520cbcce3e17c
dek <deeeqhaji@gmail.com>
2018-11-29 07:48:23 -0600

    pets commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Create an object that represents your pet.
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+const pets = {
+    name: 'coco',
+    species: 'somali',
+    nicknames: ['hairy-coco', 'sleepy-coco'],
+    age: 4
+}
+console.log(pets.nicknames)
